29cba5ee1435f9d40e578add307f900a9198d0d0
EunJung Kim <dlqnfdl@gmail.com>
2024-11-01 14:23:45 +0900

    new project

---
+++ src/test/java/com/demo/precapstone/PrecapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.precapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
